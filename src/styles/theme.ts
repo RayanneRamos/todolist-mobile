c009db66149e9da6a9043859682ed43c7d9ed2a8
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-10 21:29:29 -0300

    criando o estilos de cores da aplicação

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    GRAY_100: "#f2f2f2",
+    GRAY_200: "#d9d9d9",
+    GRAY_300: "#808080",
+    GRAY_400: "#333333",
+    GRAY_500: "#262626",
+    GRAY_600: "#1a1a1a",
+    GRAY_700: "#0d0d0d",
+
+    PURPLE_LIGHT: "#8284fa",
+    PURPLE_DARK: "#5e60ce",
+
+    BLUE_LIGHT: "#4ea8de",
+    BLUE_DARK: "#1e6f9f",
+
+    RED: "#e25858",
+  },
+};
