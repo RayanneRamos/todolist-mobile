752af88ab3c99facf0c56a3c2846a25d09219b99
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-10 21:47:11 -0300

    criando a parte do input e do button e as suas estilizações

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -8,6 +8,18 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image source={logoImage} style={styles.logo} />
+      <View style={styles.wrapper}>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Adicione uma nova tarefa"
+            placeholderTextColor={theme.colors.GRAY_300}
+          />
+          <TouchableOpacity activeOpacity={0.7} style={styles.button}>
+            <Image source={plusImage} style={styles.buttonIcon} />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -14,4 +14,45 @@ export const styles = StyleSheet.create({
     width: 110,
     height: 32,
   },
+
+  wrapper: {
+    flex: 0.8,
+    width: "100%",
+    position: "absolute",
+    bottom: 0,
+    height: "86%",
+    padding: 24,
+    backgroundColor: theme.colors.GRAY_600,
+  },
+
+  inputContainer: {
+    flexDirection: "row",
+    marginTop: -56,
+    marginBottom: 32,
+    width: "100%",
+  },
+
+  input: {
+    height: 56,
+    padding: 16,
+    borderRadius: 5,
+    flex: 1,
+    marginRight: 8,
+    color: theme.colors.GRAY_100,
+    backgroundColor: theme.colors.GRAY_500,
+  },
+
+  button: {
+    height: 56,
+    width: 56,
+    backgroundColor: theme.colors.BLUE_DARK,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 6,
+  },
+
+  buttonIcon: {
+    height: 16,
+    width: 16,
+  },
 });
