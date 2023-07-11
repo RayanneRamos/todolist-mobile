d2372a6b9b27ea48c3df5c6d2f0577cd998e60f4
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-10 22:36:28 -0300

    criando a funcionalidade do componente TaskList de adicionar uma tarefa ou remover uma tarefa

--- src/components/TaskList/index.tsx
+++ src/components/TaskList/index.tsx
@@ -1,14 +1,43 @@
-import { FlatList, Image, Text, View } from "react-native";
+import { FlatList, Image, Text, ToastAndroid, View } from "react-native";
 import { TaskItem } from "../TaskItem";
 import clipboardImage from "../../assets/clipboard.png";
 import { styles } from "./styles";
+import { ItemsProps } from "../../screens/Home";
 
-export function TaskList() {
+interface TaskListProps {
+  items: ItemsProps[];
+  setItems: (item: ItemsProps[]) => void;
+}
+
+export function TaskList({ items, setItems }: TaskListProps) {
+  const handleRemoveItem = (item: ItemsProps) => {
+    const newItems = items.filter((taskItem) => taskItem.task !== item.task);
+    setItems(newItems);
+    ToastAndroid.show("Tarefa removida", ToastAndroid.SHORT);
+  };
+
+  const handleCheckItem = (item: ItemsProps) => {
+    const updateItems = [...items];
+    const index = items.findIndex(
+      (taskItem: ItemsProps) => taskItem.task === item?.task
+    );
+    updateItems[index] = { ...item, checked: !item.checked };
+    setItems(updateItems);
+  };
+
   return (
     <View style={styles.container}>
       <FlatList
+        data={items}
         inverted
-        renderItem={() => <TaskList />}
+        keyExtractor={(item) => item?.task}
+        renderItem={({ item }) => (
+          <TaskItem
+            item={item}
+            onCheck={() => handleCheckItem(item)}
+            onRemove={() => handleRemoveItem(item)}
+          />
+        )}
         ListEmptyComponent={() => (
           <View style={styles.emptyContainer}>
             <Image source={clipboardImage} style={styles.emptyIcon} />
