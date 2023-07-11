f559b97a453f8c54e0fad363e2f5c21b041610fd
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-07-10 22:21:11 -0300

    criando toda a lógica do componente Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,57 @@
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Alert,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import logoImage from "../../assets/logo.png";
 import plusImage from "../../assets/plus.png";
 import { styles } from "./styles";
 import theme from "../../styles/theme";
 import { TaskList } from "../../components/TaskList";
+import { useCallback, useEffect, useState } from "react";
+
+export interface ItemsProps {
+  task: string;
+  checked: boolean;
+}
 
 export function Home() {
+  const [items, setItems] = useState<ItemsProps[]>([]);
+  const [inputItem, setInputItem] = useState("");
+  const [finishedItems, setFinishedItems] = useState<Number>(0);
+
+  useEffect(() => {
+    setFinishedItems(items.filter((item) => item.checked).length);
+  }, [items]);
+
+  const handleAddItem = useCallback(() => {
+    if (inputItem === "") {
+      return;
+    }
+
+    if (
+      items.find(
+        (item) => item.task.toLowerCase() === inputItem.toLocaleLowerCase()
+      )
+    ) {
+      Alert.alert(
+        "Item já existe!",
+        "Você não pode cadastrar uma tarefa com o mesmo nome"
+      );
+    }
+
+    const data = {
+      task: inputItem,
+      checked: false,
+    };
+
+    setItems((old) => [...old, data]);
+    setInputItem("");
+  }, [inputItem, setItems, items, setInputItem]);
+
   return (
     <View style={styles.container}>
       <Image source={logoImage} style={styles.logo} />
@@ -13,24 +59,30 @@ export function Home() {
         <View style={styles.inputContainer}>
           <TextInput
             style={styles.input}
+            value={inputItem}
             placeholder="Adicione uma nova tarefa"
             placeholderTextColor={theme.colors.GRAY_300}
+            onChangeText={setInputItem}
           />
-          <TouchableOpacity activeOpacity={0.7} style={styles.button}>
+          <TouchableOpacity
+            activeOpacity={0.7}
+            style={styles.button}
+            onPress={handleAddItem}
+          >
             <Image source={plusImage} style={styles.buttonIcon} />
           </TouchableOpacity>
         </View>
         <View style={styles.header}>
           <View style={styles.headerItem}>
             <Text style={styles.textBlue}>Criadas</Text>
-            <Text style={styles.valueCounter}>2</Text>
+            <Text style={styles.valueCounter}>{items?.length || 0}</Text>
           </View>
           <View style={styles.headerItem}>
             <Text style={styles.textPurple}>Concluídas</Text>
-            <Text style={styles.valueCounter}>1</Text>
+            <Text style={styles.valueCounter}>{String(finishedItems)}</Text>
           </View>
         </View>
-        <TaskList />
+        <TaskList items={items} setItems={setItems} />
       </View>
     </View>
   );
